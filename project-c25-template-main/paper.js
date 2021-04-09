53f5fa9685a7c65a8e060ea5e6844eff2ed4c825
divyanshipatel123 <divyanshipatel2008apr13@gmail.com>
2021-04-09 13:40:37 +0530

    first commit

---
+++ project-c25-template-main/paper.js
@@ -0,0 +1,39 @@
+class Paper{
+
+	constructor (x, y){
+
+	 var options={
+		 'density' : 1.5,
+		 'friction' : 1.0,
+		 'restitution' : 2.5,
+		 'isStatic' : true
+	 }
+		this.x = x;
+		this.y = y;
+		this.Width = 60;
+		this.Height = 60;
+		
+		
+		this.image=loadImage("paper.png")
+		this.body=Bodies.rectangle(this.x, this.y, this.Width, this.Height)
+		World.add(world, this.body)
+		
+
+	}
+	display()
+	{
+			var pos_y = this.body.position.y;
+			var pos_x = this.body.position.x
+			var angle = this.body.angle;
+
+			push()
+			translate(pos_x , pos_y);
+			rotate(angle)
+		    imageMode(CENTER)
+			
+			image(this.image, 0, 0, this.Width, this.Height)
+			pop()
+			
+	}
+
+}
